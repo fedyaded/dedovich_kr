781ad043bf597d487350c4699e09c2f41abfa7a5
Fiodor <107990040+fedyaded@users.noreply.github.com>
2022-08-14 18:46:05 +0300

    krded

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def card_show(num_1:str):
     print(num_1)
     print ('*' * len(num_1[:-4]) + num_1[-4:])
 
-def palindrome(slovo):
+def palindrome(slovo:str):
     slovo = slovo.replace(' ','').lower()
     if slovo == slovo[::-1]:
         print("Палиндром")
@@ -18,7 +18,7 @@ def palindrome(slovo):
 def main ():
 
     card_show("1111444477778888")
-    print(palindrome(slovo="Шабаш"))
+    print(palindrome("Шабаш"))
 
 
 
